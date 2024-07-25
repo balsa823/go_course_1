e46fedefc6ea720fa3601f8b194a2213138fc016
Balsa Popovic <balsa823@gmail.com>
2024-07-26 01:58:26 +0200

    pointers

---
+++ structs/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type contact struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstname string
+	lastname  string
+	contact
+}
+
+func main() {
+	jim := person{
+		firstname: "Jim",
+		lastname:  "Thomas",
+		contact: contact{
+			email:   "jim@gmail.com",
+			zipCode: 1500,
+		},
+	}
+	(&jim).updateName("Mark")
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) updateName(name string) {
+	(*p).firstname = name
+}
